dd1fd7b1eef4c037d2055fc9f543385d73aa4c8b
Xyenthon <42506609+Xyenthon@users.noreply.github.com>
2018-10-14 18:35:30 -0400

    Made it play a random note
    Still needs to be repeatable and show the answer when prompted to

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+//create audio context and occillator node
 
+var context = new (window.AudioContext || window.webkitAudioContext)();
+
+var o = context.createOscillator();
+var g = context.createGain();
+
+var randNote = Math.floor(Math.random()*89);
+
+function setFreq(m) {
+  o.frequency.value = Math.pow(2, (m-69)/12)*440;
+}
+
+o.type = 'sine';
+setFreq(randNote)
+o.connect(g);
+g.connect(context.destination);
+g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 3)
+o.start();
